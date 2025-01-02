7afedf0f0413b2d6e370c61a8106767e3307ecb0
snelusha <hello@snelusha.dev>
2025-01-03 04:25:22 +0530

    wip: design video preview

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,10 +1,22 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { LoaderCircleIcon, PlayIcon } from "lucide-react";
 import * as React from "react";
 
 export default function Page() {
   return (
-    <main className="relative grid min-h-[var(--main-content-height)] place-items-center">
+    <main className="relative grid min-h-[var(--main-content-height)] px-6">
+      <div className="mt-10 flex w-full flex-col items-center">
+        <div className="grid aspect-video w-full max-w-3xl place-items-center rounded-xl bg-muted">
+          <div className="flex flex-col items-center">
+            <LoaderCircleIcon className="size-5 animate-spin text-muted-foreground" />
+            <h1 className="text-base text-muted-foreground mt-4">
+              Waiting for image!
+            </h1>
+          </div>
+        </div>
+      </div>
       <footer className="absolute inset-x-0 bottom-4 flex flex-col items-center">
         <p className="text-muted-foreground">
           Made by a&nbsp;
